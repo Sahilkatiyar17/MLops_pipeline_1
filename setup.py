e364283a14f8cbe6c86c8b9c299b5ca551a3292e
Sahilkatiyar17 <sahilkatiyar2024@gmail.com>
2026-05-30 17:11:09 +0530

    project sturcture ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sahil Katiyar",
+    author_email="sahilkatiyar2024@gmail.com",
+    packages=find_packages()
+)
